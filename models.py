24eaa95b23ed548e2928e26118555effb76e5bb0
Moisés Neto <mscn@cin.ufpe.br>
2016-07-08 00:53:44 -0300

    Session model and relationship. Wrapper 'login_required' tested and working.

--- models.py
+++ models.py
@@ -4,12 +4,24 @@ from main import app
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)
-    email = db.Column('email', db.String(100))
-    username = db.Column('username', db.String(20))
-    password = db.Column('password', db.String(40)) # SHA1 encryption
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(40)) # SHA1 encryption
+    sessions = db.relationship('Session', backref='user', lazy='dynamic')
 
     def __init__(self, email, username, password):
         self.email = email
         self.username = username
         self.password = password
+
+    def __repr__(self):
+        return '<User {id}: {username}>'.format(id = self.id, username = self.username)
+
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    key = db.Column(db.String(40))
+
+    def __init__(self, user_id, key):
+        self.key = key
--- views.py
+++ views.py
@@ -1,6 +1,31 @@
 from flask import render_template, request, redirect, url_for, session, flash
 from main import app
-from models import db, User
+from models import db, User, Session
+from hashlib import sha1
+from datetime import datetime
+from time import mktime
+from functools import wraps
+
+# Wrappers
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('session_id'):
+            user_id = session.get('user_id')
+            session_id = session.get('session_id')
+            _session = Session.query.filter_by(user_id = user_id, session_id = session_id)
+
+            if _session:
+                return f(*args, **kwargs)
+
+        return redirect(url_for('login'))
+
+    return decorated_function
+
+
+
+# Views
 
 @app.route('/')
 def index():
@@ -14,22 +39,31 @@ def login():
 
         _user = User.query.filter_by(username = username).first()
 
-        if _user:
-            if _user.password == password:
-                # Do session stuff
-                session['username'] = username
-                return redirect('')
-            else:
-                flash("Wrong username or password!")
-                return render_template('login.html')
+        if _user and _user.password == password:
+            hash_key = username + app.secret_key + str(int(mktime(datetime.now().timetuple())))
+            session_id = sha1(hash_key.encode()).hexdigest()
+
+            s = Session(_user.id, session_id)
+            _user.sessions.append(s)
+            db.session.add(s)
+            db.session.commit()
+
+            session['user_id'] = _user.id
+            session['username'] = username
+            session['session_id'] = session_id
+
+            return redirect('')
         else:
-            flash("User doesn't exist!")
+            flash("Wrong username or password.")
             return render_template('login.html')
     else:
         return render_template('login.html')
 
 @app.route('/logout')
 def logout():
-    # It has a user logged in
-    session.clear()
-    return redirect('')
+    if 'session_id' in session:
+        # User is logged in
+        session.clear()
+        return redirect('')
+    else:
+        return redirect(url_for('login'))
