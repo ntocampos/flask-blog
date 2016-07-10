3dcbc532f25807c03621bbb1e7511a62890d89fb
Moisés Neto <mscn@cin.ufpe.br>
2016-07-09 21:20:52 -0300

    Adding the edit and delete methods for posts.

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from main import app
+from datetime import datetime
 
 db = SQLAlchemy(app)
 
@@ -8,7 +9,9 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(40)) # SHA1 encryption
+
     sessions = db.relationship('Session', backref='user', lazy='dynamic')
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
 
     def __init__(self, email, username, password):
         self.email = email
@@ -16,12 +19,40 @@ class User(db.Model):
         self.password = password
 
     def __repr__(self):
-        return '<User {id}: {username}>'.format(id = self.id, username = self.username)
+        return '<User [{id}]: {username}>'.format(id = self.id, username = self.username)
 
 class Session(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     key = db.Column(db.String(40))
 
-    def __init__(self, user_id, key):
+    def __init__(self, key):
         self.key = key
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(1000))
+    created_at = db.Column(db.DateTime)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        self.created_at = datetime.now()
+
+    def __repr__(self):
+        truncated = (self.title[:10] + '...') if len(self.title) > 10 else self.title
+        return 'Post [{id}]: {title}'.format(id = self.id, title = truncated)
+
+def rebuild_database():
+    db.drop_all()
+    print 'Database droped...'
+    db.create_all()
+    print 'Database created...'
+    u = User('user@example.com', 'user', 'password')
+    print 'User created...'
+
+    db.session.add(u)
+    db.session.commit()
--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for, session, flash, g
 from main import app
-from models import db, User, Session
+from models import db, User, Session, Post
 from hashlib import sha1
 from datetime import datetime
 from time import mktime
@@ -25,7 +25,8 @@ def login_required(f):
 
 @app.before_request
 def before():
-    if session.get('is_authenticated'):
+    if session.get('is_authenticated', False):
+        print 'autenticated: ' + str(session.get('is_authenticated'))
         _s = Session.query.filter_by(key = session.get('session_id')).first()
         g.current_user = _s.user
 
@@ -33,7 +34,8 @@ def before():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    _posts = Post.query.order_by('created_at DESC').all()
+    return render_template('index.html', posts = _posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -47,7 +49,7 @@ def login():
             hash_key = username + app.secret_key + str(int(mktime(datetime.now().timetuple())))
             session_id = sha1(hash_key.encode()).hexdigest()
 
-            s = Session(_user.id, session_id)
+            s = Session(session_id)
             _user.sessions.append(s)
             db.session.add(s)
             db.session.commit()
@@ -68,8 +70,59 @@ def logout():
     session.clear()
     return redirect('')
 
+@app.route('/post', methods = ['GET', 'POST'])
 @app.route('/post/new', methods = ['GET', 'POST'])
 @login_required
 def new_post():
-    if request.method == 'GET':
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        if title and body:
+            _user = g.current_user
+            _post = Post(title, body)
+            _user.posts.append(_post)
+
+            db.session.add(_user)
+            db.session.add(_post)
+            db.session.commit()
+
+            return redirect(url_for('index'))
+    else:
         return render_template('new_post.html')
+
+@app.route('/post/<int:id>')
+def view_post(id):
+    _post = Post.query.get(id)
+    if _post:
+        return render_template('post.html', post = _post)
+
+@app.route('/post/<int:id>/edit', methods = ['GET', 'POST'])
+@login_required
+def edit_post(id):
+    _post = Post.query.get(id)
+    if request.method == 'POST':
+        new_title = request.form['title']
+        new_body = request.form['body']
+
+        if new_title and new_body:
+            _post.title = new_title
+            _post.body = new_body
+            db.session.commit()
+
+        return redirect(url_for('view_post', id = id))
+    else:
+        print 'Is ownwe? ' + str(g.current_user == _post.user)
+        if g.current_user == _post.user:
+            return render_template('new_post.html', post = _post)
+        else:
+            return redirect(url_for('view_post', id = id))
+
+@app.route('/post/<int:id>/delete')
+@login_required
+def delete_post(id):
+    _post = Post.query.get(id)
+    if g.current_user == _post.user:
+        db.session.delete(_post)
+        db.session.commit()
+
+    return redirect(url_for('index'))
